7cee7aa4a711f2904ffcd7027e0249b459814ce7
Anthony Popoca <tonystr97@gmail.com>
2025-04-08 22:31:52 -0500

    screen filled with solid 'black'

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import pygame
 from constants import *
 
 def main():
+    #initialize pygame
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #created game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    pygame.display.flip()
+
+
+
+
 
 if __name__ == "__main__":
     main()
